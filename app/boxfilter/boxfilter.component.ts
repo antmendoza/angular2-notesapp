ec89a85d275d544900cab647c58f359dd8aa938b
Antonio Mendoza Pérez <antmendoza@gmail.com>
2016-12-05 18:05:34 +0000

    improve implementation

--- app/boxfilter/boxfilter.component.ts
+++ app/boxfilter/boxfilter.component.ts
@@ -2,46 +2,65 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { BoxFilterService } from './boxfilter.service';
 @Component({
     selector: 'boxfilter',
-    template: `<input name="search" [(ngModel)]="search" placeholder="search..." (keyup)="filterNotes()">
+    template: `<input name="search" [(ngModel)]="_search" placeholder="search..." (keyup)="filterNotes()">
     <span>{{suggestion()}}</span>`
 })
 
 export class BoxFilterComponent {
-    private search: string;
+    private _valueFilter: ValueFilter;
+    private _search: string;
     @Output() private boxFilterEvent = new EventEmitter<any>();
     
     private minNumLetters: number = 3;
 
     constructor() {
-        this.search = "";
+        this._search = "";
+        this._valueFilter = new ValueFilter(this._search, this.minNumLetters);
     }
 
     private suggestion() {
-        let value = this.value();
-        if (this.minNumLettersEntered() && value !== "") {
+        if (!this._valueFilter.minNumLettersEntered() && !this._valueFilter.isEmpty()) {
             return "Enter more than " + this.minNumLetters + " letter to filter";
         }
         return "";
     }
 
     private filterNotes() {
-        let value = this.value();
-        if (this.minNumLettersEntered()) {
-            value = "";
-        }
-
+        this._valueFilter = new ValueFilter(this._search, this.minNumLetters);
+        let value = this._valueFilter.valueFilter();
         this.boxFilterEvent.emit({
             value: value
         });
+        console.log(this.boxFilterEvent);
+    }
+}
+
+class ValueFilter {
+    private value: string;
+    private numLettersToSearch: number;
+    constructor(value: string, numLettersToSearch: number){
+        this.value = value;
+        this.numLettersToSearch = numLettersToSearch;
     }
 
-    private minNumLettersEntered() {
-        return this.value().length < this.minNumLetters;
+    minNumLettersEntered(){
+        return this.valueTrimed().length > this.numLettersToSearch;
     }
 
-    private value() {
-        let value = this.search.trim();
+    valueFilter(){
+        if(this.minNumLettersEntered()){
+            return this.valueTrimed();
+        }
+        return "";
+     }
+
+    isEmpty(){
+        return this.valueTrimed() === "";
+    }
+
+
+    private valueTrimed() {
+        let value = this.value.trim();
         return value;
     }
-
 }
