0f97e8ec691398bcf280e23113a907706d5971ae
Antonio Mendoza Pérez <antmendoza@gmail.com>
2017-01-05 12:53:19 +0000

    add login option to home page

--- public/app/modules/home/home.component.ts
+++ public/app/modules/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'home',
-    template: '<h1>Welcome..!! ;) </h1>'
+    templateUrl: 'home.component.html'
 })
 export class HomeComponent implements OnInit {
     constructor() { }
--- public/app/modules/home/home.module.ts
+++ public/app/modules/home/home.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; 
 import { HomeComponent }   from './home.component';
 
+
+import { AuthModule } from '../../auth/auth.module';
+
+
+
 @NgModule({
-    imports: [BrowserModule], 
+    imports: [BrowserModule, AuthModule], 
     declarations: [HomeComponent], 
     exports: [HomeComponent]
 })
